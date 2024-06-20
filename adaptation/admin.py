7354d02ec648e8961a50e4765bf7a7944222afc8
maryam abualmagd <maryamabualmajd@gmail.com>
2024-06-20 04:23:18 +0300

    added models

--- adaptation/admin.py
+++ adaptation/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from adaptation.models import backgroundColor,charSpacing,textColor,fontType
 # Register your models here.
+admin.site.register(backgroundColor)
+admin.site.register(charSpacing)
+admin.site.register(textColor)
+admin.site.register(fontType)
